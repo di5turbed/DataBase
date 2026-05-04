b8ece56cc3db10ea653c6fdbbf8e497e322e52f8
di5turbed <smirnofff.58@yandex.ru>
2026-05-04 15:06:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgvGames = new System.Windows.Forms.DataGridView();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgvPlayers = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchPlayer = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGames)).BeginInit();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 415);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(131, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Сохранить изменения";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(150, 416);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(108, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "Обновить данные";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 396);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvGames);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 370);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Игры";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgvGames
+            // 
+            this.dgvGames.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvGames.Location = new System.Drawing.Point(3, 3);
+            this.dgvGames.Name = "dgvGames";
+            this.dgvGames.Size = new System.Drawing.Size(761, 364);
+            this.dgvGames.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtSearchPlayer);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dgvPlayers);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 370);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Игроки";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgvPlayers
+            // 
+            this.dgvPlayers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlayers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPlayers.Location = new System.Drawing.Point(3, 3);
+            this.dgvPlayers.Name = "dgvPlayers";
+            this.dgvPlayers.Size = new System.Drawing.Size(761, 364);
+            this.dgvPlayers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(20, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Поиск по никнейму";
+            // 
+            // txtSearchPlayer
+            // 
+            this.txtSearchPlayer.Location = new System.Drawing.Point(23, 333);
+            this.txtSearchPlayer.Name = "txtSearchPlayer";
+            this.txtSearchPlayer.Size = new System.Drawing.Size(172, 20);
+            this.txtSearchPlayer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGames)).EndInit();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgvGames;
+        private System.Windows.Forms.DataGridView dgvPlayers;
+        private System.Windows.Forms.TextBox txtSearchPlayer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=Y:\DataBase\DataBase\MyDatabase.mdf;Integrated Security=True";
+
+        private SqlDataAdapter playersAdapter;
+        private DataTable playersTable;
+        private SqlCommandBuilder playersBuilder;
+
+        private SqlDataAdapter gamesAdapter;
+        private DataTable gamesTable;
+        private SqlCommandBuilder gamesBuilder;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += new EventHandler(Form1_Load);
+
+            buttonSave.Click += new EventHandler(buttonSave_Click);
+            buttonRefresh.Click += new EventHandler(buttonRefresh_Click);
+
+            txtSearchPlayer.TextChanged += new EventHandler(txtSearchPlayer_TextChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadPlayers();
+            LoadGames();
+        }
+
+        private void LoadPlayers()
+        {
+            try
+            {
+                string query = "SELECT id, nickname, first_name, last_name, phone, reg_date, date_of_birth FROM Players";
+                playersAdapter = new SqlDataAdapter(query, connectionString);
+                playersBuilder = new SqlCommandBuilder(playersAdapter);
+                playersTable = new DataTable();
+
+                playersAdapter.Fill(playersTable);
+
+                dgvPlayers.DataSource = playersTable;
+
+                if (dgvPlayers.Columns["id"] != null) dgvPlayers.Columns["id"].Visible = false;
+
+                dgvPlayers.Columns["nickname"].HeaderText = "Никнейм";
+                dgvPlayers.Columns["first_name"].HeaderText = "Имя";
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка игроков: " + ex.Message); }
+        }
+
+        private void LoadGames()
+        {
+            try
+            {
+                string query = "SELECT id, name, category, notes FROM Game";
+                gamesAdapter = new SqlDataAdapter(query, connectionString);
+                gamesBuilder = new SqlCommandBuilder(gamesAdapter);
+                gamesTable = new DataTable();
+
+                gamesAdapter.Fill(gamesTable);
+
+                dgvGames.DataSource = gamesTable;
+
+                if (dgvGames.Columns["id"] != null) dgvGames.Columns["id"].Visible = false;
+
+                dgvGames.Columns["name"].HeaderText = "Название игры";
+                dgvGames.Columns["category"].HeaderText = "Категория";
+                dgvGames.Columns["notes"].HeaderText = "Заметки";
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка игр: " + ex.Message); }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvPlayers.IsCurrentCellInEditMode) dgvPlayers.EndEdit();
+                if (dgvGames.IsCurrentCellInEditMode) dgvGames.EndEdit();
+
+                if (playersAdapter != null && playersTable != null) playersAdapter.Update(playersTable);
+                if (gamesAdapter != null && gamesTable != null) gamesAdapter.Update(gamesTable);
+
+                MessageBox.Show("Данные успешно сохранены в БД!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                LoadPlayers();
+                LoadGames();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сохранении: " + ex.Message);
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            LoadPlayers();
+            LoadGames();
+        }
+
+        private void txtSearchPlayer_TextChanged(object sender, EventArgs e)
+        {
+            if (playersTable != null)
+            {
+                playersTable.DefaultView.RowFilter = string.Format("nickname LIKE '%{0}%'", txtSearchPlayer.Text);
+            }
+        }
+    }
+}
