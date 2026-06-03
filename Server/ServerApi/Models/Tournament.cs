68006906012aad02ccd5959f8207171616449fe9
di5turbed <smirnofff.58@yandex.ru>
2026-06-03 15:59:32 +0300

    1

---
+++ Server/ServerApi/Models/Tournament.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServerApi.Models
+{
+    [Table("tournament")]
+    public class Tournament
+    {
+        [Column("id")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column("begin_date")]
+        public DateTime BeginDate { get; set; }
+
+        [Column("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("prizepool")]
+        public int Prizepool { get; set; }
+    }
+}
--- Server/ServerApi/Program.cs
+++ Server/ServerApi/Program.cs
@@ -35,3 +35,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+public partial class Program { }
